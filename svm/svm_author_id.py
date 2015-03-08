7c76a3974ee4c2333fb29c6c00ac25d1c504ae1f
Peter Lai <peter.yd.lai@gmail.com>
2015-03-07 23:42:25 -0500

    svm exercises

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,41 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import pdb
+
+clf = svm.SVC(kernel='rbf',C=10000.0)
+
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+
+# train
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+# predict
+t1 = time()
+predicted = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+print accuracy_score(labels_test, predicted)
+
+# predicting individual entries
+
+# ten = clf.predict(features_test[10])
+# print ten
+
+# twosix = clf.predict(features_test[26])
+# print twosix
+
+# fifty = clf.predict(features_test[50])
+# print fifty
+
+# counting test events predicted to be chris
+print sum(predicted==1)
+
 #########################################################
 
 
